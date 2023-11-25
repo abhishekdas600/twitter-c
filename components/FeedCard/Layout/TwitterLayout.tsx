dc2c643f87a77286ea0a285044ffce3a6b9d6ed6
abhishekdas <dasabhshek522@gmail.com>
2023-11-25 20:38:43 +0530

    made responsive design

---
+++ components/FeedCard/Layout/TwitterLayout.tsx
@@ -0,0 +1,119 @@
+import React, { useCallback } from "react";
+import Image from "next/image";
+import { CgProfile } from "react-icons/cg";
+import { GoBell } from "react-icons/go";
+import { HiOutlineClipboardList } from "react-icons/hi";
+import { MdHomeFilled, MdOutlineMailOutline } from "react-icons/md";
+import { RiSearchLine, RiTwitterXFill } from "react-icons/ri";
+import { useCurrentUser } from "@/hooks/user";
+import toast from "react-hot-toast";
+import { graphqlClient } from "@/clients/api";
+import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
+import { useQueryClient } from "@tanstack/react-query";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+
+interface TwitterLayoutProps {
+    children: React.ReactNode
+}
+interface TwitterSidebarButton{
+    title: string,
+    icon: React.ReactNode
+  }
+
+const sidebarMenuItems: TwitterSidebarButton[] =[
+    {
+      title: 'Home',
+      icon: <MdHomeFilled />
+    },
+    {
+      title: 'Explore',
+      icon: <RiSearchLine />
+    },
+    {
+      title: 'Notifications',
+      icon: <GoBell />
+    },
+    {
+      title: 'Messages',
+      icon: <MdOutlineMailOutline/>
+    },
+    {
+      title: 'Lists',
+      icon: <HiOutlineClipboardList />
+    },
+    {
+      title: 'Profile',
+      icon: <CgProfile />
+    }
+  ]
+
+const TwitterLayout: React.FC<TwitterLayoutProps>= (props) => {
+
+    const {user} = useCurrentUser(); 
+    const queryClient = useQueryClient();   
+    const handleLoginWithGoogle = useCallback(async (cred : CredentialResponse) => {
+        const googleToken = cred.credential
+        if(!googleToken){
+          return toast.error('Google token not found');
+        }
+        const {verifyGoogleToken} = await graphqlClient.request(verifyUserGoogleTokenQuery, {token : googleToken});
+        toast.success('Verified Success')
+        console.log(verifyGoogleToken);
+    
+        if(verifyGoogleToken) {
+          window.localStorage.setItem('twitter_clone_token', verifyGoogleToken);
+        }
+        await queryClient.invalidateQueries({queryKey: ["current-user"]});
+      },[queryClient])
+    
+
+    return (
+        <div>
+           <div>
+      <div className='  grid grid-cols-12 h-screen w-screen '>
+        <div className='col-span-2 sm:col-span-4  pt-5 flex justify-end '>
+          
+          <div className="mt-1 text-2xl  pl-2 ">
+          <div className=' text-3xl hover:bg-slate-600 rounded-full h-fit w-fit p-2 pl-5 sm:pl-0 cursor-pointer transition-all'>
+          <RiTwitterXFill />
+          </div>
+            <ul>
+            {sidebarMenuItems.map(item => {
+              return (
+                <li className="flex justify-start items-center gap-5  hover:bg-slate-600 rounded-full h-fit w-fit py-2 pl-5 sm:pl-2 pr-2 sm:pr-7 cursor-pointer transition-all " key={item.title}><span>{item.icon}</span><span className="hidden sm:inline">{item.title}</span></li>
+              )
+            })}
+            </ul>
+            <button className="hidden sm:block bg-blue-500  py-3 rounded-full mt-4 mb-20 w-[74%] font-semibold  hover:bg-blue-400">Post</button>
+            <button className="block sm:hidden ml-2 sm:ml-0 bg-blue-500  p-3 rounded-full mt-4 mb-20  font-semibold  hover:bg-blue-400"><RiTwitterXFill /></button>
+            {user && <div className="pr-5 sm:pr-0 mt-20  flex  gap-3 px-2  py-3 rounded-full text-xl hover:bg-slate-700 cursor-pointer transition-all">
+          {user && user.profileImageUrl &&
+          <Image className=" rounded-full "
+          src={user?.profileImageUrl} 
+          alt="User-image" 
+          height={50} 
+          width={50}/> }
+          <h3 className="hidden sm:inline">{user.firstName} {user.lastName}</h3>
+         </div>}  
+          </div>
+         
+        
+          
+        </div>
+        <div className='col-span-10 sm:col-span-6 lg:col-span-4 border-[1px] border-gray-500 overflow-y-scroll no-scrollbar'>
+            {props.children}
+        </div>
+        <div className='hidden lg:col-span-4 '>
+          {!user&&<div className="border p-6 mr-20 mt-5 ml-3 bg-slate-700 rounded-lg">
+            <h4>New to Twitter?</h4>
+          <GoogleLogin onSuccess={handleLoginWithGoogle}/>
+          </div>}
+          
+        </div>
+      </div>
+    </div>
+        </div>
+    )
+}
+
+export default TwitterLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,116 +1,43 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useState } from "react"
 import Image from 'next/image'
-import {RiSearchLine, RiTwitterXFill} from "react-icons/ri"
-import {MdHomeFilled, MdOutlineMailOutline} from "react-icons/md"
-import {GoBell, GoImage} from "react-icons/go"
-import {HiOutlineClipboardList} from "react-icons/hi"
-import {CgProfile} from "react-icons/cg"
+import {GoImage} from "react-icons/go"
 import FeedCard from "@/components/FeedCard"
-import { CredentialResponse, GoogleLogin } from "@react-oauth/google"
-import toast from "react-hot-toast"
-import { graphqlClient } from "@/clients/api"
-import { verifyUserGoogleTokenQuery } from "@/graphql/query/user"
 import { useCurrentUser } from "@/hooks/user"
-import { useQueryClient } from "@tanstack/react-query"
-import { useGetAllTweets } from "@/hooks/tweet"
+import { useCreateTweet, useGetAllTweets } from "@/hooks/tweet"
 import { Tweet } from "@/gql/graphql"
-
-
-interface TwitterSidebarButton{
-  title: string,
-  icon: React.ReactNode
-}
-
-const sidebarMenuItems: TwitterSidebarButton[] =[
-  {
-    title: 'Home',
-    icon: <MdHomeFilled />
-  },
-  {
-    title: 'Explore',
-    icon: <RiSearchLine />
-  },
-  {
-    title: 'Notifications',
-    icon: <GoBell />
-  },
-  {
-    title: 'Messages',
-    icon: <MdOutlineMailOutline/>
-  },
-  {
-    title: 'Lists',
-    icon: <HiOutlineClipboardList />
-  },
-  {
-    title: 'Profile',
-    icon: <CgProfile />
-  }
-]
+import TwitterLayout from "@/components/FeedCard/Layout/TwitterLayout";
 
 export default function Home() {
 
   const {user} = useCurrentUser();
   const {tweets = []} = useGetAllTweets();
+  const {mutate} = useCreateTweet();
 
-  const queryClient = useQueryClient();
+  
+  const [content, setContent] = useState("")
   
   const handleSelectImage = useCallback(()=>{
     const input = document.createElement('input')
     input.setAttribute('type', 'file');
     input.setAttribute('accept', 'image/*');
     input.click();
-  }, [])
+  }, []);
 
-   const handleLoginWithGoogle = useCallback(async (cred : CredentialResponse) => {
-    const googleToken = cred.credential
-    if(!googleToken){
-      return toast.error('Google token not found');
-    }
-    const {verifyGoogleToken} = await graphqlClient.request(verifyUserGoogleTokenQuery, {token : googleToken});
-    toast.success('Verified Success')
-    console.log(verifyGoogleToken);
+  const handleCreateTweet = useCallback(()=>{
+    mutate({
+      content
+    })
 
-    if(verifyGoogleToken) {
-      window.localStorage.setItem('twitter_clone_token', verifyGoogleToken);
-    }
-    await queryClient.invalidateQueries({queryKey: ["current-user"]});
-  },[queryClient])
+    setContent("");
+  },[content, mutate])
+
+   
 
   return (
+   <div>
+    <TwitterLayout>
     <div>
-      <div>
-      <div className='grid grid-cols-12 h-screen w-screen px-36'>
-        <div className='col-span-3  pt-5 '>
-          <div className=' text-3xl hover:bg-slate-600 rounded-full h-fit w-fit p-2 cursor-pointer transition-all'>
-          <RiTwitterXFill />
-          </div>
-          <div className="mt-3 text-2xl  pl-2 ">
-            <ul>
-            {sidebarMenuItems.map(item => {
-              return (
-                <li className="flex justify-start items-center gap-5  hover:bg-slate-600 rounded-full h-fit w-fit py-2 pl-2 pr-7 cursor-pointer transition-all " key={item.title}><span>{item.icon}</span><span>{item.title}</span></li>
-              )
-            })}
-            </ul>
-            
-          </div>
-         <div>
-         <button className="bg-blue-500  py-3 rounded-full mt-4 mb-20 w-[74%] font-semibold  hover:bg-blue-400">Post</button>
-         </div>
-         {user && <div className="mt-20 ml-10 flex  gap-3 px-3 mr-12 py-3 rounded-full  hover:bg-slate-700 cursor-pointer transition-all">
-          {user && user.profileImageUrl &&
-          <Image className="rounded-full"
-          src={user?.profileImageUrl} 
-          alt="User-image" 
-          height={50} 
-          width={50}/> }
-          <h3>{user.firstName} {user.lastName}</h3>
-         </div>}  
-          
-        </div>
-        <div className='col-span-5 border-[1px] border-gray-500 overflow-y-scroll no-scrollbar'>
-          {user?.profileImageUrl && <div  className="border-t border-gray-500 p-4 hover:bg-gray-800 cursor-pointer transition-all grid grid-cols-12">
+       {user?.profileImageUrl && <div  className="border-t border-gray-500 p-4 hover:bg-gray-800 cursor-pointer transition-all grid grid-cols-12">
               <div className="col-span-1">
                  <Image src={user?.profileImageUrl}
                 className="rounded-full"
@@ -120,6 +47,8 @@ export default function Home() {
               </div>
               <div className="col-span-11 pl-4">
                  <textarea 
+                 value={content}
+                 onChange={(e)=> setContent(e.target.value)}
                  className="w-full bg-transparent text-lg p-3 border-b border-slate-700 overflow-visible"
                  rows={3}
                  placeholder="What is happening?!">
@@ -129,7 +58,7 @@ export default function Home() {
                   <GoImage onClick={handleSelectImage}  className="text-xl"/>
                   </div>
                   <div>
-                  <button className="bg-blue-500 rounded-full font-semibold  hover:bg-blue-400 px-4 py-1">Post</button>
+                  <button onClick={handleCreateTweet} className="bg-blue-500 rounded-full font-semibold  hover:bg-blue-400 px-4 py-1">Post</button>
                   </div>
                  </div>
               </div>
@@ -138,17 +67,9 @@ export default function Home() {
             {tweets?.map(tweet => tweet? <FeedCard key={tweet?.id} data = {tweet as Tweet}/>: null)}
            
           </div>
-        </div>
-        <div className='col-span-4 '>
-          {!user&&<div className="border p-6 mr-20 mt-5 ml-3 bg-slate-700 rounded-lg">
-            <h4>New to Twitter?</h4>
-          <GoogleLogin onSuccess={handleLoginWithGoogle}/>
-          </div>}
-          
-        </div>
-      </div>
     </div>
-    </div>
+    </TwitterLayout>
+   </div>
   )
 }
 
